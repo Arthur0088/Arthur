4306928d690360b4984a27bfdd8dfcdad1d928e9
Arthur0088 <63612886+Arthur0088@users.noreply.github.com>
2020-04-13 17:20:23 -0300

    upload dos exercícios

---
+++ operadores.java
@@ -0,0 +1,33 @@
+package primeiraaula;
+
+public class operadores {
+
+	public static void main(String[] args) {
+		 
+	int x=12;
+	int y=5;
+	
+	int soma=x+y;
+	int subtracao=x-y;
+	int multiplicacao=x*y;
+	int divisaoInteira=x/y;
+	double divisaoComum=(double)x/y;
+	int modulo=x%y;
+	
+
+	System.out.println("Soma ="+soma);
+	System.out.println("Subtração ="+subtracao);
+	System.out.println("Multiplicação ="+multiplicacao);
+	System.out.println("Divisão Inteira ="+divisaoInteira);
+	System.out.println("Divisão Comum="+divisaoComum);
+	System.out.println("Modulo ="+modulo);
+	
+		
+		
+		
+		
+		
+		
+	}
+
+}
